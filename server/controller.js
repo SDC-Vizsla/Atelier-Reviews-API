a1f4d7d4b0c2ec41faf907434a9d09b208c81c62
Sean Jung <sean.jung23@gmail.com>
2023-06-01 16:14:29 -0400

    all queries now working

--- server/controller.js
+++ server/controller.js
@@ -14,174 +14,181 @@ module.exports = {
 
     if (!req.query.product_id || req.query.product_id > 1000011) {
       return res.sendStatus(500);
-    } else {
-      const product_id = req.query.product_id;
-      const page = Number(req.query.page) || 1;
-      const count = Number(req.query.count) || 5;
-      const sort = req.query.sort;
+    }
 
-      Reviews.findAll({
-        where: {
-          product_id: product_id
+    const product_id = req.query.product_id;
+    const page = Number(req.query.page) || 1;
+    const count = Number(req.query.count) || 5;
+    const sort = req.query.sort;
+
+    Reviews.findAll({
+      where: {
+        product_id: product_id,
+      },
+      include: [
+        {
+          model: ReviewPhotos,
         },
-        include: [{
-          model: ReviewPhotos
-        }],
-        limit: count,
-        offset: (page - 1) * count,
-        order: [['helpfulness', 'DESC']]
-      })
-        .then((reviews) => {
-          let queryResponse = {
-            product: product_id,
-            page: page,
-            count: count,
-            results: []
+      ],
+      limit: count,
+      offset: (page - 1) * count,
+      order: [["helpfulness", "DESC"]],
+    })
+      .then((reviews) => {
+        let queryResponse = {
+          product: product_id,
+          page: page,
+          count: count,
+          results: [],
+        };
+
+        reviews.map((review) => {
+          let reviewTemp = {
+            review_id: review.dataValues.id,
+            rating: review.dataValues.rating,
+            summary: review.dataValues.summary,
+            recommend: review.dataValues.recommend,
+            response: review.dataValues.response,
+            body: review.dataValues.body,
+            date: review.dataValues.date,
+            reviewer_name: review.dataValues.reviewer_name,
+            helpfulness: review.dataValues.helpfulness,
+            photos: [],
           };
 
-          reviews.map((review) => {
-            let reviewTemp = {
-              review_id: review.dataValues.id,
-              rating: review.dataValues.rating,
-              summary: review.dataValues.summary,
-              recommend: review.dataValues.recommend,
-              response: review.dataValues.response,
-              body: review.dataValues.body,
-              date: review.dataValues.date,
-              reviewer_name: review.dataValues.reviewer_name,
-              helpfulness: review.dataValues.helpfulness,
-              photos: []
+          review.dataValues.review_photos.map((photo) => {
+            let photoTemp = {
+              id: photo.id,
+              url: photo.url,
             };
 
-            review.dataValues.review_photos.map((photo) => {
-              let photoTemp = {
-                id: photo.id,
-                url: photo.url
-              };
-
-              reviewTemp.photos.push(photoTemp);
-            });
-
-            reviewTemp.photos.sort((a, b) => a.id - b.id);
-            queryResponse.results.push(reviewTemp);
+            reviewTemp.photos.push(photoTemp);
           });
 
-          if (sort === 'newest') {
-            queryResponse.results.sort((a, b) => new Date(b.date) - new Date(a.date));
-          } else if (sort === 'relevant') {
-            queryResponse.results.sort((a, b) => b.body.length - a.body.length);
-          }
-
-          res.send(queryResponse);
-
-          const endTime = performance.now();
-          const time = ((endTime - startTime) / 1000).toFixed(3);
-          console.log('time elapsed during search query: ', time);
-        })
-        .catch((err) => {
-          console.error('Error retrieving reviews!:', err);
-          res.sendStatus(500);
+          reviewTemp.photos.sort((a, b) => a.id - b.id);
+          queryResponse.results.push(reviewTemp);
         });
-    }
+
+        if (sort === "newest") {
+          queryResponse.results.sort(
+            (a, b) => new Date(b.date) - new Date(a.date)
+          );
+        } else if (sort === "relevant") {
+          queryResponse.results.sort((a, b) => b.body.length - a.body.length);
+        }
+
+        res.send(queryResponse);
+
+        const endTime = performance.now();
+        const time = ((endTime - startTime) / 1000).toFixed(3);
+        console.log("time elapsed during search query: ", time);
+      })
+      .catch((err) => {
+        console.error("Error retrieving reviews!:", err);
+        res.sendStatus(500);
+      });
   },
   getReviewsMeta: (req, res) => {
     const startTime = performance.now();
 
     if (!req.query.product_id || req.query.product_id > 1000011) {
       return res.sendStatus(500);
-    } else {
-      const product_id = req.query.product_id;
-
-      Reviews.findAll({
-        where: {
-          product_id: product_id
-        },
-        attributes: [
-          'rating',
-          'recommend',
-          [sequelize.fn('COUNT', sequelize.col('rating')), 'ratingCount'],
-          [sequelize.fn('SUM', sequelize.literal('CASE WHEN recommend = true THEN 1 ELSE 0 END')), 'recommendTrueCount'],
-          [sequelize.fn('SUM', sequelize.literal('CASE WHEN recommend = false THEN 1 ELSE 0 END')), 'recommendFalseCount']
-        ],
-        group: ['rating', 'recommend']
-      })
-        .then((reviews) => {
-          let metaResponse = {
-            product_id: product_id,
-            ratings: {},
-            recommended: {},
-            characteristics: {}
-          };
-
-          reviews.forEach((review) => {
-            // Ratings
-            const rating = review.dataValues.rating.toString();
-            const ratingCount = review.dataValues.ratingCount;
-            metaResponse.ratings[rating] = ratingCount.toString();
-
-            // Recommended
-            const recommend = review.dataValues.recommend ? true : false;
-            const recommendCount = recommend ? parseInt(review.dataValues.recommendTrueCount) : parseInt(review.dataValues.recommendFalseCount);
-
-            if (recommend) {
-              metaResponse.recommended.true = (metaResponse.recommended.true || 0) + recommendCount;
-            } else {
-              metaResponse.recommended.false = (metaResponse.recommended.false || 0) + recommendCount;
-            }
-          });
-
-          if (metaResponse.recommended.true) {
-            metaResponse.recommended.true = metaResponse.recommended.true.toString();
-          }
-          if (metaResponse.recommended.false){
-            metaResponse.recommended.false = metaResponse.recommended.false.toString();
-          }
-
-          Characteristics.findAll({
-            where: {
-              product_id: product_id
-            },
-            include: [
-              {
-                model: CharacteristicReviews,
-                attributes: ['value']
-              }
-            ],
-            attributes: ['id', 'name'],
-            group: ['characteristics.id', 'characteristics.name', 'characteristic_reviews.id', 'characteristic_reviews.value']
-          })
-            .then((characteristics) => {
-              characteristics.forEach((characteristic) => {
-                const charId = characteristic.dataValues.id;
-                const charName = characteristic.dataValues.name;
-                const charAverage = parseFloat(characteristic.dataValues.characteristic_reviews[0].dataValues.value).toFixed(16);
-
-                metaResponse.characteristics[charName] = {
-                  id: charId,
-                  value: charAverage.toString()
-                };
-              });
-
-              res.send(metaResponse);
-
-              const endTime = performance.now();
-              const time = ((endTime - startTime) / 1000).toFixed(3);
-              console.log('time elapsed during search query: ', time);
-            })
-            .catch((err) => {
-              console.error('Error retrieving characteristic reviews!:', err);
-              res.sendStatus(500);
-            });
-        })
-        .catch((err) => {
-          console.error('Error retrieving reviews!:', err);
-          res.sendStatus(500);
-        });
     }
+
+    const product_id = req.query.product_id;
+
+    Reviews.findAll({
+      where: {
+        product_id: product_id,
+      },
+      attributes: [
+        "rating",
+        "recommend",
+        [sequelize.fn("COUNT", sequelize.col("rating")), "ratingCount"],
+        [sequelize.fn("SUM", sequelize.literal("CASE WHEN recommend = true THEN 1 ELSE 0 END")), "recommendTrueCount"],
+        [sequelize.fn("SUM", sequelize.literal("CASE WHEN recommend = false THEN 1 ELSE 0 END")), "recommendFalseCount"],
+      ],
+      group: ["rating", "recommend"],
+    })
+      .then((reviews) => {
+        let metaResponse = {
+          product_id: product_id,
+          ratings: {},
+          recommended: {},
+          characteristics: {},
+        };
+
+        reviews.forEach((review) => {
+          const rating = review.dataValues.rating.toString();
+          const ratingCount = review.dataValues.ratingCount;
+          const recommend = review.dataValues.recommend ? true : false;
+          const recommendCount = recommend ? parseInt(review.dataValues.recommendTrueCount) : parseInt(review.dataValues.recommendFalseCount);
+
+          metaResponse.ratings[rating] = ratingCount.toString();
+
+          if (recommend) {
+            metaResponse.recommended.true = (metaResponse.recommended.true || 0) + recommendCount;
+          } else {
+            metaResponse.recommended.false = (metaResponse.recommended.false || 0) + recommendCount;
+          }
+        });
+
+        if (metaResponse.recommended.true) {
+          metaResponse.recommended.true = metaResponse.recommended.true.toString();
+        }
+        if (metaResponse.recommended.false) {
+          metaResponse.recommended.false = metaResponse.recommended.false.toString();
+        }
+
+        Characteristics.findAll({
+          where: {
+            product_id: product_id,
+          },
+          include: [
+            {
+              model: CharacteristicReviews,
+              attributes: ["value"],
+            },
+          ],
+          attributes: ["id", "name"],
+          group: [
+            "characteristics.id",
+            "characteristics.name",
+            "characteristic_reviews.id",
+            "characteristic_reviews.value",
+          ],
+        })
+          .then((characteristics) => {
+            characteristics.forEach((characteristic) => {
+              const charId = characteristic.dataValues.id;
+              const charName = characteristic.dataValues.name;
+              const charAverage = parseFloat(characteristic.dataValues.characteristic_reviews[0].dataValues.value).toFixed(16);
+
+              metaResponse.characteristics[charName] = {
+                id: charId,
+                value: charAverage.toString(),
+              };
+            });
+
+            res.send(metaResponse);
+
+            const endTime = performance.now();
+            const time = ((endTime - startTime) / 1000).toFixed(3);
+            console.log("time elapsed during search query: ", time);
+          })
+          .catch((err) => {
+            console.error("Error retrieving characteristic reviews!:", err);
+            res.sendStatus(500);
+          });
+      })
+      .catch((err) => {
+        console.error("Error retrieving reviews!:", err);
+        res.sendStatus(500);
+      });
   },
   postReview: (req, res) => {
+    const startTime = performance.now();
 
-    // console.log('this is req', req.body);
     if (
       !req.body.product_id ||
       !req.body.rating ||
@@ -225,61 +232,68 @@ module.exports = {
             if (photos.length > 0) {
               ReviewPhotos.count()
                 .then((photoCount) => {
-                  console.log('thisis photoCount', photoCount)
                   photos.map((photo) => {
                     ReviewPhotos.create({
                       id: photoCount += 1,
                       review_id: review.dataValues.id,
                       url: photo
                     })
-                      .catch((err) => console.error('', err))
+                      .catch((err) => console.error('Error creating new review photos!: ', err));
                   })
                 })
-                .catch((err) => console.error('Error counting review_photos rows!: ', err))
+                .catch((err) => console.error('Error counting review_photos rows!: ', err));
             }
 
+            CharacteristicReviews.count()
+              .then((characteristicReviewsCount) => {
+                let temp = Object.keys(characteristics);
 
+                if (temp.length > 0) {
+                  temp.map((char_id) => {
+                    CharacteristicReviews.create({
+                      id: characteristicReviewsCount += 1,
+                      characteristic_id: parseInt(char_id),
+                      review_id: review.dataValues.id,
+                      value: characteristics[char_id]
+                    })
+                      .catch((err) => console.error('Error creating new Characteristics Review!', err));
+                  })
+                }
+
+                res.sendStatus(201);
+
+                const endTime = performance.now();
+                const time = ((endTime - startTime) / 1000).toFixed(3);
+                console.log("time elapsed during search query: ", time);
+              })
+              .catch((err) => console.error(err));
           })
-          .catch((err) => console.error('Error creating new Review!: ', err))
+          .catch((err) => console.error('Error creating new Review!: ', err));
       })
       .catch((err) => console.error('Error counting reviews rows!: ', err));
   },
   upvoteReview: (req, res) => {
+    if (!req.params.review_id) {
+      return res.sendStatus(400);
+    }
 
+    Reviews.increment(
+      {helpfulness: 1},
+      {where: {id: Number(req.params.review_id)}}
+    )
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => console.error('Error upvoting review!: ', err));
   },
   reportReview: (req, res) => {
-
+    Reviews.update(
+      {reported: true},
+      {where: { id: Number(req.params.review_id) }}
+    )
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => console.error('Error reporting review!: ', err));
   }
 };
-
-
-  // .then((review) => {
-  //   console.log('hjfkashfkjashfa', review);
-  //   reviewId = review.id; // Store the ID of the newly created review
-
-  //   // Create the review photos in the ReviewPhotos table
-  //   const photoPromises = photos.map((photo) =>
-  //     ReviewPhotos.create({
-  //       review_id: reviewId,
-  //       url: photo,
-  //     })
-  //   );
-
-  //   return Promise.all(photoPromises); // Wait for all photo creations to complete
-  // })
-  // .then(() => {
-  //   // Create/update the characteristic reviews in the CharacteristicReviews table
-  //   const characteristicPromises = Object.entries(characteristics).map(([charId, value]) =>
-  //     CharacteristicReviews.findOrCreate({
-  //       where: {
-  //         review_id: reviewId,
-  //         characteristic_id: charId,
-  //       },
-  //       defaults: {
-  //         value: value,
-  //       },
-  //     })
-  //   );
-
-  //   return Promise.all(characteristicPromises); // Wait for all characteristic creations/updates to complete
-  // })
